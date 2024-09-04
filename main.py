d9133003d2e0938388abe585f23cd2f34d4da286
ArtemHolodilov <art.holodilov@gmail.com>
2024-09-05 03:30:35 +0800

    Query params and their validation.

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Query, Path
 
 
 app = FastAPI()
 
 
-# @app.get("/users/{id}")
-# def users(id: int):
-# 	return {"user_id": id}
+# @app.get("/users")
+# def get_model(name:str, age:int):
+# 	return {"user_name": name, "user_age": age}
 
-# @app.get("/users/{name}")
-# def users(name:str = Path(min_length=3, max_length=20)):
+# @app.get("/users")
+# def users(name:str = Query(min_length=3, max_length=20)):
 # 	return {"name": name}
 
-@app.get("/users/{name}/{age}")
+# @app.get("/users")
+# def users(name:str = Query(min_length=3, max_length=20), 
+# 			age:int = Query(ge=18, lt=111)):
+# 	return {"name": name, "age": age}
+
+# @app.get("/users")
+# def users(people: list[str] = Query()):
+# 	return {"people": people}
+
+@app.get("/users/{name}")
 def users(name:str = Path(min_length=3, max_length=20),
-			age:int = Path(ge=18, lt=111)):
+			age:int = Query(ge=18, lt=111)):
 	return {"name": name, "age": age}
