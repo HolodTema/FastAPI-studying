90536ed9e1b438e4f7ae407833ed9250fe1fd572
ArtemHolodilov <art.holodilov@gmail.com>
2024-09-05 03:15:51 +0800

    Path parameters and their validation.

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Path
+
 
 app = FastAPI()
 
 
-# @app.get("/")
-# def root():
-# 	return FileResponse("index.html")
+# @app.get("/users/{id}")
+# def users(id: int):
+# 	return {"user_id": id}
 
-# @app.get("/file", response_class = FileResponse)
-# def root_html():
-# 	return "index.html"
-	
-@app.get("/")
-def root():
-	return FileResponse("index.html", filename="mainpage.html", media_type="application/octet-stream")
+# @app.get("/users/{name}")
+# def users(name:str = Path(min_length=3, max_length=20)):
+# 	return {"name": name}
 
+@app.get("/users/{name}/{age}")
+def users(name:str = Path(min_length=3, max_length=20),
+			age:int = Path(ge=18, lt=111)):
+	return {"name": name, "age": age}
