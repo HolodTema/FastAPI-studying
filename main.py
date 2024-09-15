d959890020e84872f5fa4fa5681d632cf89963b3
ArtemHolodilov <art.holodilov@gmail.com>
2024-09-16 05:26:04 +0800

    processing POST request body using POJOs from pydantic lib.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
+
+
+class Person(BaseModel): 
+    name: str = Field(default="Undefined", min_length=3, max_length=30)
+    age: int = Field(default = "18", ge=18, lt=111)
 
 app = FastAPI()
 
@@ -8,8 +14,6 @@ def root():
     return FileResponse("public/index.html")
 
 @app.post("/hello")
-def hello(data = Body()):
-    name = data["name"]
-    age = data["age"]
-    return {"message": f"{name}, your age is {age}"}
+def hello(person: Person):
+    return {"message": f"Hello, {person.name}! Your age is {person.age}."}
 
